4ffe5a1e927b9b8eadf690e440da5b6f859a03d3
Manollo Guedes <manollo.guedes11@gmail.com>
2020-08-10 00:01:00 -0300

    feat: initial spring application commit
    Adding project dependencies, settings and build files

---
+++ src/test/java/com/guedes/herlon/game/TenPinBowlingScoreGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guedes.herlon.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenPinBowlingScoreGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
