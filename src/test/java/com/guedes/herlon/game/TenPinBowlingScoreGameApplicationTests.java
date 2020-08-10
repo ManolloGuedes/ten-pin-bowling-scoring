7ff84c156dac2b019e9a881f113ca64716ffb62e
Manollo Guedes <manollo.guedes11@gmail.com>
2020-08-10 10:40:04 -0300

    test: creating application test

--- src/test/java/com/guedes/herlon/game/TenPinBowlingScoreGameApplicationTests.java
+++ src/test/java/com/guedes/herlon/game/TenPinBowlingScoreGameApplicationTests.java
@@ -1,13 +1,20 @@
 package com.guedes.herlon.game;
 
+import com.guedes.herlon.game.controller.GameController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TenPinBowlingScoreGameApplicationTests {
 
+	@Autowired
+	private GameController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
