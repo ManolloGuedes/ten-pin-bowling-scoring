115f5a83fd5ea47fd5b5b5301bbb6725d569f83c
Manollo Guedes <manollo.guedes11@gmail.com>
2020-08-12 20:13:55 -0300

    test: creating tests for perfect, zero and fault scores

--- src/test/java/com/guedes/herlon/game/service/GameServiceImplTest.java
+++ src/test/java/com/guedes/herlon/game/service/GameServiceImplTest.java
@@ -6,14 +6,11 @@ import com.guedes.herlon.game.model.interfaces.Frame;
 import com.guedes.herlon.game.model.interfaces.Game;
 import com.guedes.herlon.game.model.interfaces.PlayerThrow;
 import com.guedes.herlon.game.service.interfaces.GameService;
-import org.junit.jupiter.api.BeforeEach;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,14 +26,14 @@ class GameServiceImplTest {
     private GameController gameController;
     private Game gameTest;
 
-    @BeforeEach
-    void init() throws IOException {
-        Path filePath = Paths.get("src/test/resources/", "test.txt");
-        gameTest = gameService.createGameUsing(gameController.getThrowDetailsFrom(filePath.toString()));
+    @SneakyThrows
+    private void initUsingInputFile(String fileName) {
+        gameTest = gameService.createGameUsing(gameController.getThrowDetailsFrom(fileName));
     }
 
     @Test
     void creatingGame() {
+        initUsingInputFile("src/test/resources/test.txt");
         assertAll(
                 () -> assertTrue(gameTest.hasPlayer("Jeff")),
                 () -> assertTrue(gameTest.hasPlayer("John")),
@@ -46,6 +43,7 @@ class GameServiceImplTest {
 
     @Test
     void verifyFrameAmount() {
+        initUsingInputFile("src/test/resources/test.txt");
         List<Frame> jeffFrames = getFramesUsing("Jeff");
 
         List<Frame> johnFrames = getFramesUsing("John");
@@ -58,6 +56,7 @@ class GameServiceImplTest {
 
     @Test
     void verifyStrikes() {
+        initUsingInputFile("src/test/resources/test.txt");
         List<Frame> jeffFrames = getFramesUsing("Jeff");
 
         List<Frame> johnFrames = getFramesUsing("John");
@@ -78,6 +77,7 @@ class GameServiceImplTest {
 
     @Test
     void verifySpares() {
+        initUsingInputFile("src/test/resources/test.txt");
         List<Frame> jeffFrames = getFramesUsing("Jeff");
 
         List<Frame> johnFrames = getFramesUsing("John");
@@ -96,7 +96,8 @@ class GameServiceImplTest {
     }
 
     @Test
-    void calculateFinalResult() {
+    void calculateFinalResultSimpleGame() {
+        initUsingInputFile("src/test/resources/test.txt");
         gameService.calculateFinalResultOf(gameTest);
 
         List<Frame> jeffFrames = getFramesUsing("Jeff");
@@ -106,6 +107,36 @@ class GameServiceImplTest {
                 () -> assertEquals(167, jeffFrames.get(jeffFrames.size()-1).getScore()),
                 () -> assertEquals(151, johnFrames.get(jeffFrames.size()-1).getScore())
         );
+    }
+
+    @Test
+    void calculateFinalResultPerfectScore() {
+        initUsingInputFile("src/test/resources/perfect-score.txt");
+        gameService.calculateFinalResultOf(gameTest);
+
+        List<Frame> carlFrames = getFramesUsing("Carl");
+
+        assertEquals(300, carlFrames.get(carlFrames.size()-1).getScore());
+    }
+
+    @Test
+    void calculateFinalResultZeroScore() {
+        initUsingInputFile("src/test/resources/zero-score.txt");
+        gameService.calculateFinalResultOf(gameTest);
+
+        List<Frame> carlFrames = getFramesUsing("Carl");
+
+        assertEquals(0, carlFrames.get(carlFrames.size()-1).getScore());
+    }
+
+    @Test
+    void calculateFaultScore() {
+        initUsingInputFile("src/test/resources/fault-score.txt");
+        gameService.calculateFinalResultOf(gameTest);
+
+        List<Frame> carlFrames = getFramesUsing("Carl");
+
+        assertEquals(0, carlFrames.get(carlFrames.size()-1).getScore());
     }
 
     private List<Frame> getFramesUsing(String playerName) {
